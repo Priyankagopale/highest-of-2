6c06cf62fa8ce97d439902dfaec30b53c22d04de
Priyankagopale <99202174+Priyankagopale@users.noreply.github.com>
2022-02-07 22:05:44 +0530

    highest of 2

---
+++ highest of 2.py
@@ -0,0 +1,8 @@
+num1= int(input("Enter number 1: "))
+num2= int(input("Enter number 2: "))
+if (num1==num2):
+    print("Both the numbers are equal")
+elif(num1>num2):
+    print("Number 1 is greater than Number 2")
+else:
+    print("Number 2 is gretaer than Number 1")
